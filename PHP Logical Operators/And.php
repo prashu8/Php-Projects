8fa390b85254573a329fb0966c09f7290e57885c
Prashansa <prashansa.dhanke@appxbuild.com>
2020-03-11 18:03:27 +0530

    complete operator

---
+++ PHP Logical Operators/And.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 and $y == 50) {
+    echo "Hello world!";
+}
+?>  
+
+</body>
+</html>
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 && $y == 50) {
+    echo "Hello world!";
+}
+?>  
+
+</body>
+</html> -->
---
+++ PHP Logical Operators/Or.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 or $y == 80) {
+    echo "Hello world!";
+}
+?>  
+
+</body>
+</html>
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 100;  
+$y = 50;
+
+if ($x == 100 || $y == 80) {
+    echo "Hello world!";
+}
+?>  
+
+</body>
+</html> -->
